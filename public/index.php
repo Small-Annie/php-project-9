15c734ac0263129dfe791207b2b93e7b2ad3697c
Petrova Anna <petr230999@gmail.com>
2026-01-06 13:07:24 +0300

    Add main page template and render it in index.php

--- public/index.php
+++ public/index.php
@@ -3,12 +3,15 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Page Analyzer!');
-    return $response;
+$renderer = new PhpRenderer(__DIR__ . '/../templates');
+
+$app->get('/', function ($request, $response) use ($renderer) {
+    return $renderer->render($response, 'index.phtml');
 });
+
 $app->run();
